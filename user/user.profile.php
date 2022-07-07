6337cbc5723ccbf4327caed30797584a5b3beca4
fajrun <fajrunss7@gmail.com>
2022-07-07 10:06:11 +0700

    add footer

---
+++ user/user.profile.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../owner/assets/icons/css/all.min.css">
+</head>
+
+<body>
+    <main>
+
+    </main>
+    <footer class="container bg-light text-center pt-3">
+        <!-- Grid container -->
+        <div class="container">
+            <!-- Section: Social media -->
+            <div class="row">
+                <div class="col">
+                    <section class="social-media mb-4">
+                        <h3 class=" pb-5 fw-bold">Social Media</h3>
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-3">
+                                <!-- Facebook -->
+                                <a class="social-media-ref" href="#FacebookDEKOST">
+                                    <i class="fa-brands fa-facebook me-2"></i>Facebook
+                                </a>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-3">
+                                <!-- Twitter -->
+                                <a class="social-media-ref" href="#TwitterDEKOST">
+                                    <i class="fa-brands fa-twitter me-2 "></i>Twitter
+                                </a>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-3">
+                                <!-- Instagram -->
+                                <a class="social-media-ref" href="#InstagramDEKOST">
+                                    <i class="fa-brands fa-instagram me-2"></i>Instagram
+                                </a>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-3">
+                                <!-- Linkedin -->
+                                <a class="social-media-ref" href="#LinkedInDEKOST">
+                                    <i class="fa-brands fa-linkedin me-2"></i>LinkedIn
+                                </a>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <div class="col">
+                    <section class="contact mb-4">
+                        <h3 class="header-footer pb-5">Contact me</h3>
+                        <div class="row">
+                            <div class="col-12 col-sm-6 col-md-6">
+                                <a class="social-media-ref" href="#FacebookDEKOST">
+                                    <i class="fa-regular fa-envelope me-2"></i>Dekost@gmail.com
+                                </a>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-6">
+                                <i class="fab fa-whatsapp-square me-2"></i> +6281668909890
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            2022 © Copryright <a class="text-white" href="#dekost.com">DEKOST</a> - All rights reserved - Made in Yogyakarta
+        </div>
+    </footer>
+
+
+
+</body>
+
+</html>
--- user/user.transaksi.php
+++ user/user.transaksi.php
@@ -13,10 +13,71 @@
     <!-- Favicon -->
     <link rel="icon" href="assets/icon/favicon.ico">
     <title>Document</title>
+    <link rel="stylesheet" href="../owner/assets/icons/css/all.min.css">
 </head>
 
 <body>
 
 </body>
+
+<footer class="container bg-light text-center pt-3">
+    <!-- Grid container -->
+    <div class="container">
+        <!-- Section: Social media -->
+        <div class="row">
+            <div class="col">
+                <section class="social-media mb-4">
+                    <h3 class=" pb-5 fw-bold">Social Media</h3>
+                    <div class="row">
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <!-- Facebook -->
+                            <a class="social-media-ref" href="#FacebookDEKOST">
+                                <i class="fa-brands fa-facebook me-2"></i>Facebook
+                            </a>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <!-- Twitter -->
+                            <a class="social-media-ref" href="#TwitterDEKOST">
+                                <i class="fa-brands fa-twitter me-2 "></i>Twitter
+                            </a>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <!-- Instagram -->
+                            <a class="social-media-ref" href="#InstagramDEKOST">
+                                <i class="fa-brands fa-instagram me-2"></i>Instagram
+                            </a>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-3">
+                            <!-- Linkedin -->
+                            <a class="social-media-ref" href="#LinkedInDEKOST">
+                                <i class="fa-brands fa-linkedin me-2"></i>LinkedIn
+                            </a>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <div class="col">
+                <section class="contact mb-4">
+                    <h3 class="header-footer pb-5">Contact me</h3>
+                    <div class="row">
+                        <div class="col-12 col-sm-6 col-md-6">
+                            <a class="social-media-ref" href="#FacebookDEKOST">
+                                <i class="fa-regular fa-envelope me-2"></i>Dekost@gmail.com
+                            </a>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-6">
+                            <i class="fab fa-whatsapp-square me-2"></i> +6281668909890
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        2022 © Copryright <a class="text-white" href="#dekost.com">DEKOST</a> - All rights reserved - Made in Yogyakarta
+    </div>
+</footer>
 
 </html>
