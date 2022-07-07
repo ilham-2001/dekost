5f442c55ed6f5e51e344e25b75566082bb0104c2
fajrun <fajrunss7@gmail.com>
2022-07-07 20:58:25 +0700

    membuat tampilan owner signin dan signup

--- owner/owner.signup.php
+++ owner/owner.signup.php
@@ -70,7 +70,126 @@ if (isset($_POST['button_signup'])) {
 
 <body>
 
-    <main class="container-fluid">
+    <!-- MAIN BARU -->
+    <main>
+        <section class="section-sign-up" style="background-color: #ffffff;">
+            <div class="container py-5">
+                <div class="row d-flex justify-content-center align-items-center">
+                    <div class="col-xl-10">
+                        <div class="card rounded-3 text-black">
+                            <div class="row">
+                                <div class="col-lg-6 d-flex align-items-center gradient-custom-2 bg-primary">
+                                    <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" data-bs-interval=3500>
+                                            <div class="carousel-indicators">
+                                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                            </div>
+                                            <div class="carousel-inner">
+                                                <div class="carousel-item active">
+                                                    <img src="assets/app/images/login.png" class="d-block w-100" alt="...">
+                                                    <div class="carousel-caption d-none d-md-block">
+                                                        <h5>First slide label</h5>
+                                                        <p>Some representative placeholder content for the first slide.</p>
+                                                    </div>
+                                                </div>
+                                                <div class="carousel-item">
+                                                    <img src="assets/app/images/login2.png" class="d-block w-100" alt="...">
+                                                    <div class="carousel-caption d-none d-md-block">
+                                                        <h5>Second slide label</h5>
+                                                        <p>Some representative placeholder content for the second slide.</p>
+                                                    </div>
+                                                </div>
+                                                <div class="carousel-item">
+                                                    <img src="assets/app/images/login3.png" class="d-block w-100" alt="...">
+                                                    <div class="carousel-caption d-none d-md-block">
+                                                        <h5>Third slide label</h5>
+                                                        <p>Some representative placeholder content for the third slide.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                <span class="visually-hidden">Previous</span>
+                                            </button>
+                                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                <span class="visually-hidden">Next</span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="card-body p-md-5">
+                                        <div class="text-center">
+                                            <h4> <img class="pb-2 pe-2" src="../owner/assets/icons/logo.png" style="width: 50px; height:50px;" alt="logo">De'Kost</h4>
+                                        </div>
+                                        <form class="form-signin" method="POST">
+                                            <p class="fw-bold text-center">CREATE ACCOUNT</p>
+                                            <div class="form-floating">
+                                                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email" autocomplete="off">
+                                                <label for="floatingInput">Email address</label>
+                                            </div>
+
+                                            <div class="form-floating">
+                                                <input type="password" class="form-control" id="floatingPassword" placeholder="password" name="password" autocomplete="off">
+                                                <label for="floatingPassword">Password</label>
+                                            </div>
+                                            <div class="row mb-2">
+                                                <div class="col">
+                                                    <div class="form-floating">
+                                                        <input type="text" class="form-control" id="floatingInput" placeholder="nama depan" name="nama_depan" autocomplete="off">
+                                                        <label for="floatingInput">First Name</label>
+                                                    </div>
+                                                </div>
+                                                <div class="col">
+                                                    <div class="form-floating">
+                                                        <input type="text" class="form-control" id="floatingInput" placeholder="nama belakang" name="nama_belakang" autocomplete="off">
+                                                        <label for="floatingInput">Last Name</label>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-floating">
+                                                <input type="text" class="form-control" id="floatingInput" placeholder="NIK" name="nik" autocomplete="off">
+                                                <label for="floatingInput">NIK</label>
+                                            </div>
+
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault1" value="L">
+                                                <label class="form-check-label" for="flexRadioDefault1">
+                                                    Male
+                                                </label>
+                                            </div>
+                                            <div class="form-check form-check-inline">
+                                                <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault2" checked value="P">
+                                                <label class="form-check-label" for="flexRadioDefault2">
+                                                    Female
+                                                </label>
+                                            </div>
+                                            <button class="w-100 btn btn-lg btn-primary btn-login mt-2" type="submit" name="button_signup">Sign
+                                                Un
+                                            </button>
+                                            <div class="d-flex align-items-center justify-content-center">
+                                                <p class="me-2 mt-3 ms-2">Have an account?</p>
+                                                <a class="user-signup" href="owner.login.php">Sign In</a>
+                                            </div>
+                                        </form>
+                                        <p class="mb-2 text-center" style="color:#2155CD ;">&copy;2022 De'kost All rights reserved</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+    <!-- MAIN LAMA -->
+    <!-- <main class="container-fluid">
 
         <div class="row align-items-start">
             <div class="col">
@@ -82,45 +201,38 @@ if (isset($_POST['button_signup'])) {
                     <form class="form-signin" method="POST">
 
                         <div class="form-floating">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
-                                name="email" autocomplete="off">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email" autocomplete="off">
                             <label for="floatingInput">Email address</label>
                         </div>
 
                         <div class="form-floating">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="password"
-                                name="password" autocomplete="off">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="password" name="password" autocomplete="off">
                             <label for="floatingPassword">Password</label>
                         </div>
 
                         <div class="form-floating">
-                            <input type="text" class="form-control" id="floatingInput" placeholder="nama depan"
-                                name="nama_depan" autocomplete="off">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="nama depan" name="nama_depan" autocomplete="off">
                             <label for="floatingInput">Nama Depan</label>
                         </div>
 
                         <div class="form-floating">
-                            <input type="text" class="form-control" id="floatingInput" placeholder="nama belakang"
-                                name="nama_belakang" autocomplete="off">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="nama belakang" name="nama_belakang" autocomplete="off">
                             <label for="floatingInput">Nama Belakang</label>
                         </div>
 
                         <div class="form-floating">
-                            <input type="text" class="form-control" id="floatingInput" placeholder="NIK" name="nik"
-                                autocomplete="off">
+                            <input type="text" class="form-control" id="floatingInput" placeholder="NIK" name="nik" autocomplete="off">
                             <label for="floatingInput">NIK</label>
                         </div>
 
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault1"
-                                value="L">
+                            <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault1" value="L">
                             <label class="form-check-label" for="flexRadioDefault1">
                                 Laki-laki
                             </label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault2"
-                                checked value="P">
+                            <input class="form-check-input" type="radio" name="jenis_kelamin" id="flexRadioDefault2" checked value="P">
                             <label class="form-check-label" for="flexRadioDefault2">
                                 Perempuan
                             </label>
@@ -134,7 +246,7 @@ if (isset($_POST['button_signup'])) {
         </div>
 
 
-    </main>
+    </main> -->
 
 
     <script src="assets/app/js/bootstrap.bundle.min.js"></script>
