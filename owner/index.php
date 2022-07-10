e56cfa0bf1a605ae9ebd0ec20d0ab246fbfd78c5
ilham-2001 <arizqyakbar@gmail.com>
2022-07-10 15:44:54 +0700

    add

--- owner/index.php
+++ owner/index.php
@@ -214,8 +214,9 @@ $dataPoints2 = array(
                                                     <div class="col mr-2">
                                                         <div
                                                             class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                            hmm</div>
-                                                        <div class="h5 mb-0 font-weight-bold text-gray-800">3</div>
+                                                            Jumlah Kost</div>
+                                                        <div class="h5 mb-0 font-weight-bold text-gray-800">(count data
+                                                            kos)</div>
                                                     </div>
                                                     <div class="col-auto">
                                                         <i class="fas fa-calendar fa-2x text-gray-300"></i>
@@ -233,8 +234,9 @@ $dataPoints2 = array(
                                                     <div class="col mr-2">
                                                         <div
                                                             class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                            Y</div>
-                                                        <div class="h5 mb-0 font-weight-bold text-gray-800">1m</div>
+                                                            JUmlah Kamar</div>
+                                                        <div class="h5 mb-0 font-weight-bold text-gray-800">(count data
+                                                            kamar)</div>
                                                     </div>
                                                     <div class="col-auto">
                                                         <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -252,13 +254,13 @@ $dataPoints2 = array(
                                                     <div class="col mr-2">
                                                         <div
                                                             class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                                            hi
+                                                            Jumlah Penghuni
                                                         </div>
                                                         <div class="row no-gutters align-items-center">
                                                             <div class="col-auto">
                                                                 <div
                                                                     class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
-                                                                    2</div>
+                                                                    (count data penyewa)</div>
                                                             </div>
                                                             <div class="col">
                                                                 <div class="progress progress-sm mr-2">
