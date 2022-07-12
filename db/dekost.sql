7eb94c55532cf04e9589c3c47c1cc725acd49912
muhammadnovidr <20523122@students.uii.ac.id>
2022-07-12 22:32:32 +0700

    data-dummy

--- db/dekost.sql
+++ db/dekost.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jul 12, 2022 at 09:08 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Host: 127.0.0.1
+-- Generation Time: Jul 12, 2022 at 05:30 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -188,7 +188,144 @@ INSERT INTO `kamar` (`id_kost`, `status`, `lebar`, `panjang`, `idKamar`) VALUES
 (8, 'KOSONG', 3, 4, '62cd1a9167f91'),
 (8, 'KOSONG', 3, 4, '62cd1a916945b'),
 (8, 'KOSONG', 3, 4, '62cd1a916a705'),
-(8, 'KOSONG', 3, 4, '62cd1a916ba1e');
+(8, 'KOSONG', 3, 4, '62cd1a916ba1e'),
+(9, 'KOSONG', 3, 4, '62cd86cc72268'),
+(9, 'KOSONG', 3, 4, '62cd86cc7296b'),
+(9, 'KOSONG', 3, 4, '62cd86cc73058'),
+(9, 'KOSONG', 3, 4, '62cd86cc73796'),
+(9, 'KOSONG', 3, 4, '62cd86cc73e8f'),
+(9, 'KOSONG', 3, 4, '62cd86cc74463'),
+(9, 'KOSONG', 3, 4, '62cd86cc74a10'),
+(9, 'KOSONG', 3, 4, '62cd86cc74f6b'),
+(10, 'KOSONG', 3, 4, '62cd883c9338b'),
+(10, 'KOSONG', 3, 4, '62cd883c93a26'),
+(10, 'KOSONG', 3, 4, '62cd883c93ecc'),
+(10, 'KOSONG', 3, 4, '62cd883c9447f'),
+(10, 'KOSONG', 3, 4, '62cd883c951d0'),
+(10, 'KOSONG', 3, 4, '62cd883c956b4'),
+(10, 'KOSONG', 3, 4, '62cd883c95cdb'),
+(10, 'KOSONG', 3, 4, '62cd883c96322'),
+(11, 'KOSONG', 3, 4, '62cd892712de4'),
+(11, 'KOSONG', 3, 4, '62cd89271345d'),
+(11, 'KOSONG', 3, 4, '62cd892713bcf'),
+(11, 'KOSONG', 3, 4, '62cd8927142fa'),
+(11, 'KOSONG', 3, 4, '62cd89271491c'),
+(11, 'KOSONG', 3, 4, '62cd892715013'),
+(12, 'KOSONG', 3, 4, '62cd89b3417c7'),
+(12, 'KOSONG', 3, 4, '62cd89b341e0f'),
+(12, 'KOSONG', 3, 4, '62cd89b342dc4'),
+(12, 'KOSONG', 3, 4, '62cd89b34330d'),
+(12, 'KOSONG', 3, 4, '62cd89b3437c5'),
+(12, 'KOSONG', 3, 4, '62cd89b343cdd'),
+(12, 'KOSONG', 3, 4, '62cd89b344261'),
+(12, 'KOSONG', 3, 4, '62cd89b344818'),
+(12, 'KOSONG', 3, 4, '62cd89b344d02'),
+(12, 'KOSONG', 3, 4, '62cd89b3451c6'),
+(13, 'KOSONG', 3, 4, '62cd8abd51f00'),
+(13, 'KOSONG', 3, 4, '62cd8abd526b7'),
+(13, 'KOSONG', 3, 4, '62cd8abd52c77'),
+(13, 'KOSONG', 3, 4, '62cd8abd5324f'),
+(13, 'KOSONG', 3, 4, '62cd8abd53894'),
+(13, 'KOSONG', 3, 4, '62cd8abd549f1'),
+(13, 'KOSONG', 3, 4, '62cd8abd55082'),
+(13, 'KOSONG', 3, 4, '62cd8abd555b3'),
+(13, 'KOSONG', 3, 4, '62cd8abd55abd'),
+(13, 'KOSONG', 3, 4, '62cd8abd55fb8'),
+(13, 'KOSONG', 3, 4, '62cd8abd564a5'),
+(13, 'KOSONG', 3, 4, '62cd8abd569ce'),
+(13, 'KOSONG', 3, 4, '62cd8abd56f2b'),
+(13, 'KOSONG', 3, 4, '62cd8abd57557'),
+(13, 'KOSONG', 3, 4, '62cd8abd57b03'),
+(13, 'KOSONG', 3, 4, '62cd8abd5805c'),
+(14, 'KOSONG', 3, 4, '62cd8b76699f9'),
+(14, 'KOSONG', 3, 4, '62cd8b7669f74'),
+(14, 'KOSONG', 3, 4, '62cd8b766a489'),
+(14, 'KOSONG', 3, 4, '62cd8b766aa3f'),
+(14, 'KOSONG', 3, 4, '62cd8b766b819'),
+(14, 'KOSONG', 3, 4, '62cd8b766bd29'),
+(14, 'KOSONG', 3, 4, '62cd8b766c1b9'),
+(14, 'KOSONG', 3, 4, '62cd8b766c698'),
+(14, 'KOSONG', 3, 4, '62cd8b766cc6c'),
+(14, 'KOSONG', 3, 4, '62cd8b766d1ef'),
+(14, 'KOSONG', 3, 4, '62cd8b766d84d'),
+(14, 'KOSONG', 3, 4, '62cd8b766defe'),
+(14, 'KOSONG', 3, 4, '62cd8b766e481'),
+(14, 'KOSONG', 3, 4, '62cd8b766ea64'),
+(14, 'KOSONG', 3, 4, '62cd8b766efea'),
+(15, 'KOSONG', 3, 4, '62cd8d747db62'),
+(15, 'KOSONG', 3, 4, '62cd8d747e197'),
+(15, 'KOSONG', 3, 4, '62cd8d747e641'),
+(15, 'KOSONG', 3, 4, '62cd8d747eb58'),
+(15, 'KOSONG', 3, 4, '62cd8d747f02a'),
+(15, 'KOSONG', 3, 4, '62cd8d747f4e3'),
+(15, 'KOSONG', 3, 4, '62cd8d7480474'),
+(15, 'KOSONG', 3, 4, '62cd8d7480b0e'),
+(15, 'KOSONG', 3, 4, '62cd8d74810ba'),
+(15, 'KOSONG', 3, 4, '62cd8d74815e4'),
+(15, 'KOSONG', 3, 4, '62cd8d7481c13'),
+(15, 'KOSONG', 3, 4, '62cd8d74822aa'),
+(15, 'KOSONG', 3, 4, '62cd8d7482820'),
+(15, 'KOSONG', 3, 4, '62cd8d7482d4a'),
+(15, 'KOSONG', 3, 4, '62cd8d7483339'),
+(15, 'KOSONG', 3, 4, '62cd8d74838ad'),
+(15, 'KOSONG', 3, 4, '62cd8d7483e4d'),
+(15, 'KOSONG', 3, 4, '62cd8d74845fc'),
+(16, 'KOSONG', 3, 4, '62cd8f0880ab4'),
+(16, 'KOSONG', 3, 4, '62cd8f0880f90'),
+(16, 'KOSONG', 3, 4, '62cd8f08814cb'),
+(16, 'KOSONG', 3, 4, '62cd8f08819a7'),
+(16, 'KOSONG', 3, 4, '62cd8f0881ee0'),
+(16, 'KOSONG', 3, 4, '62cd8f0882457'),
+(16, 'KOSONG', 3, 4, '62cd8f08833de'),
+(16, 'KOSONG', 3, 4, '62cd8f0883a68'),
+(16, 'KOSONG', 3, 4, '62cd8f0883f73'),
+(16, 'KOSONG', 3, 4, '62cd8f08843fe'),
+(16, 'KOSONG', 3, 4, '62cd8f088487d'),
+(16, 'KOSONG', 3, 4, '62cd8f0884d43'),
+(16, 'KOSONG', 3, 4, '62cd8f088527d'),
+(16, 'KOSONG', 3, 4, '62cd8f08857ea'),
+(16, 'KOSONG', 3, 4, '62cd8f0885cf8'),
+(17, 'KOSONG', 3, 4, '62cd8fa29152f'),
+(17, 'KOSONG', 3, 4, '62cd8fa291ae4'),
+(17, 'KOSONG', 3, 4, '62cd8fa292098'),
+(17, 'KOSONG', 3, 4, '62cd8fa29258a'),
+(17, 'KOSONG', 3, 4, '62cd8fa292bf9'),
+(17, 'KOSONG', 3, 4, '62cd8fa2934a8'),
+(17, 'KOSONG', 3, 4, '62cd8fa293ad3'),
+(17, 'KOSONG', 3, 4, '62cd8fa29412b'),
+(17, 'KOSONG', 3, 4, '62cd8fa2946e5'),
+(17, 'KOSONG', 3, 4, '62cd8fa294c9b'),
+(17, 'KOSONG', 3, 4, '62cd8fa295426'),
+(17, 'KOSONG', 3, 4, '62cd8fa2964d7'),
+(17, 'KOSONG', 3, 4, '62cd8fa296b42'),
+(18, 'KOSONG', 3, 4, '62cd9032740c7'),
+(18, 'KOSONG', 3, 4, '62cd9032745a9'),
+(18, 'KOSONG', 3, 4, '62cd903274b0e'),
+(18, 'KOSONG', 3, 4, '62cd9032750bb'),
+(18, 'KOSONG', 3, 4, '62cd90327564b'),
+(18, 'KOSONG', 3, 4, '62cd903275c3e'),
+(18, 'KOSONG', 3, 4, '62cd9032761e7'),
+(18, 'KOSONG', 3, 4, '62cd9032769ab'),
+(18, 'KOSONG', 3, 4, '62cd9032770dd'),
+(18, 'KOSONG', 3, 4, '62cd903277719'),
+(18, 'KOSONG', 3, 4, '62cd903277cf0'),
+(18, 'KOSONG', 3, 4, '62cd90327818d'),
+(18, 'KOSONG', 3, 4, '62cd903278709'),
+(18, 'KOSONG', 3, 4, '62cd903278c44'),
+(19, 'KOSONG', 3, 4, '62cd90f4499d7'),
+(19, 'KOSONG', 3, 4, '62cd90f44b1de'),
+(19, 'KOSONG', 3, 4, '62cd90f44b950'),
+(19, 'KOSONG', 3, 4, '62cd90f44be73'),
+(19, 'KOSONG', 3, 4, '62cd90f44c340'),
+(19, 'KOSONG', 3, 4, '62cd90f44c7cc'),
+(19, 'KOSONG', 3, 4, '62cd90f44cd88'),
+(19, 'KOSONG', 3, 4, '62cd90f44db8d'),
+(19, 'KOSONG', 3, 4, '62cd90f44e211'),
+(19, 'KOSONG', 3, 4, '62cd90f44e780'),
+(19, 'KOSONG', 3, 4, '62cd90f44eca8'),
+(19, 'KOSONG', 3, 4, '62cd90f44f1a4'),
+(19, 'KOSONG', 3, 4, '62cd90f44f64e'),
+(19, 'KOSONG', 3, 4, '62cd90f44fb4e');
 
 -- --------------------------------------------------------
 
@@ -218,7 +355,18 @@ INSERT INTO `kost` (`id`, `alamat`, `nama`, `jumlahKamar`, `NIK_Pemilik`, `harga
 (4, 'Jl. kaliurang, Krawitan, Umbulmartani, Ngemplak', 'bebas', 10, '12378678362736', 700000, 'Putra', '62c978ced7192.jpg'),
 (5, 'Cost Putra Sadewa Pugeran, Maguwoharjo, Depok, Sleman', 'Kost Sadewa', 20, '8907134586971234', 560000, 'Putra', '62cb6b0bcd1c1.jpg'),
 (6, 'dekost', 'dekost', 21, '1234523476123467', 234000, 'Putra', '62cd1682354d4.jpg'),
-(8, 'Jl.Kaliurang Km 13.5', 'Elhaus', 21, '1234890723456789', 234000, 'Putra', '62cd1a914b59b.jpg');
+(8, 'Jl.Kaliurang Km 13.5', 'Elhaus', 21, '1234890723456789', 234000, 'Putra', '62cd1a914b59b.jpg'),
+(9, 'saaxdasdsa', 'saasa', 8, '1232132312312', 1000000, 'Putri', '62cd86cc71750.jpg'),
+(10, 'Jl. Kimpulan No.2, Krawitan, Umbulmartani, Kec. Ngemplak, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55584', 'Kost Putra Omah Barokah', 8, '210501212120001', 1200000, 'Putra', '62cd883c91577.jpg'),
+(11, '8C88+HWF, Ngemplak, Umbulmartani, Kec. Ngemplak, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55584', 'Kost Cakra Putra', 6, '210501111110001', 100000, 'Putra', '62cd892711c5a.jpg'),
+(12, 'Gg. Jati Jl. Degolan, Ngemplak, Umbulmartani, Kec. Ngemplak, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55584', 'Kost Putri Eksklusif RUMAHKU', 10, '210502222220001', 700000, 'Putri', '62cd89b340aed.jpg'),
+(13, 'Ngemplak, Umbulmartani, Kec. Ngemplak, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55584', 'Kost Joglo Puri Anggrek II', 16, '210503333330001', 1300000, 'Putra', '62cd8abd501f3.jpg'),
+(14, '8C86+8WR, Jl. Kimpulan, Krawitan, Umbulmartani, Kec. Ngemplak, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55584', 'Kost Putri Diajeng 88', 15, '210504444440001', 750000, 'Putri', '62cd8b7668860.jpg'),
+(15, 'Jl. Degolan No.3, Ngemplak, Umbulmartani, Kec. Ngemplak, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55584', 'Kost putra MHR', 18, '210505555550001', 1000000, 'Putra', '62cd8d747cff0.jpg'),
+(16, '8C98+92R, Unnamed Road, Kopatan, Umbulmartani, Kec. Ngemplak, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55584', 'Kost Putri Orenz', 15, '210506666660001', 1000000, 'Putri', '62cd8f087f664.jpg'),
+(17, 'Jalan Kaliurang KM. 15, Umbulmartani, Ngemplak, Umbulmartani, Kec. Ngemplak, Kabupaten Sleman, Daerah Istimewa Yogyakart', 'Kost Arra', 13, '210507777770001', 800000, 'Putri', '62cd8fa29085c.jpg'),
+(18, '8C89+R48, Gg. Mahoni, Ngemplak, Umbulmartani, Kec. Ngemplak, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55584', 'Kost Putra Anugrah', 14, '210508888880001', 950000, 'Putra', '62cd9032733f4.jpg'),
+(19, '8C97+6C8, Kopatan, Krawitan, Krawitan, Umbulmartani, Kec. Ngemplak, Kabupaten Sleman, Daerah Istimewa Yogyakarta 55584', 'Kost Fadillah ', 14, '210509999990001', 1000000, '', '62cd90f448cbc.jpg');
 
 -- --------------------------------------------------------
 
@@ -240,12 +388,24 @@ CREATE TABLE `pemilik` (
 --
 
 INSERT INTO `pemilik` (`NIK`, `nama`, `noTelp`, `alamat`, `email`, `keypassword`) VALUES
+('1232132312312', 'as sa', '2312321312312', 'saaasa', 'asaasaas@gmail.com', '$2y$10$5wt5wuBAnPRQfwiSGCwvP.tfIvCvK2PxSIQYSZIwQtiDV.c7UVNtq'),
 ('1234096789123456', 'test test', '123456789123', 'test ', 'test@gmail.com', '$2y$10$ym51t82wfJFxA79UYaK4c.RRONg1VVPuNU9Y6i0U9iv8F/tt9zoH6'),
 ('1234123412341234', 'corn wall', '081234561234', 'jl. kaliurang km 12.5', 'corn@gmail.com', '$2y$10$kBiuyx229Si5Ecx23wXqweF4KrxXjHlBr2YIAIs2J1fTokRungcPu'),
 ('1234523476123467', 'halo bandung', '123498567123', 'jl bandung', 'bandung@gmail.com', '$2y$10$dBrFJYFigbGU2k5R/O0OruPynUBAP.cQ1sB3BTKw/AATAmxgYQ/9u'),
 ('1234890723456789', 'dara zara', '091234567890', 'Tanggerang Selatan', 'dara@gmail.com', '$2y$10$WlOtfRF8zfHkEsVGanoP8eFDPCnnfEzQ7AfUNd3W1fRlrcQHRsANq'),
 ('1235908712345678', 'corn wall', '081234567895', 'corn field', 'corn@gmail.com', '$2y$10$gZ1WNbAeRNJSxYdMaBTNFurNAZYBQONLEwKHKkYxSNwPJ3NYN7pH.'),
 ('12378678362736', 'budi cek', '081227875674', 'kaliurang', 'cek@gmail.com', '$2y$10$HNqdJ1ZMRFsSQSQwUgY0/OxHU.FCcg8mmF6cmqlRJOL94mcfK1KgS'),
+('210501111110001', 'Oliva Mardhiyah', '(+62) 820 047 7', 'Ki. Siliwangi No. 170, Madiun 17273, Babel', 'vsaputra@rajasa.desa.id', '$2y$10$tOmS4qp0dL6ymSTYZeiFYOW32P/qdFN86mqyYodmdVVW1U99H2f6q'),
+('210501212120001', 'Wulan Kusmawati', '020 6257 7570', 'Jln. Wahid Hasyim No. 693, Parepare 59308, Jateng', 'qnajmudin@saragih.in', '$2y$10$zWcwnOsVSlNY2NwgMAnHaOW6JMYwBtKPlX3u9mN6mNV8mHKt2qoXm'),
+('210501222220001', 'Harto  Winarno', '(+62) 343 4499 ', 'Dk. Sentot Alibasa No. 561, Manado 37326, Jateng', 'umaya36@yahoo.com', '$2y$10$RuDHF7tu9aHkzbOyYvYSaelXJ88I1ogcZpTEszSNf7g6oasMWY6LS'),
+('210502222220001', 'Silvia Pertiwi', '0572 5652 0693', 'Gg. Sutoyo No. 77, Tarakan 36695, Banten', 'amalia47@yahoo.co.id', '$2y$10$eUAYHMYj2dqYJY3aw2B/mePOT9H/6aJ0d9a2rUu9xo9W5uDZ2CFyK'),
+('210503333330001', 'Yosef  Saefullah', '0664 6798 0714', 'Ds. HOS. Cjokroaminoto (Pasirkaliki) No. 599, Jambi 89597, Maluku', 'wibisono.warta@gmail.com', '$2y$10$MsHNk4l3yESq048pAVIEXOmH920hI9GTBMTfRz/IMk4PQKBL5TJN.'),
+('210504444440001', 'Mahesa  Olga', '0848 3703 062', 'Psr. Nakula No. 929, Bima 18404, Sulbar', 'mandasari.salimah@yahoo.com', '$2y$10$NWgJ3s402kwisfGFsKBcI.60eAfwv3fwC96pK/VjgX5ObyG9G9u1u'),
+('210505555550001', 'Tomi Tampubolon', '0825 2553 5363', 'Gg. Baha No. 486, Depok 13718, DIY', 'darsirah27@gmail.com', '$2y$10$YOtrd08suP6CB/qDdan12uWIDgADNAbYoyO0ELUjhEMCxvW.ESj12'),
+('210506666660001', 'Argono  Anggriawan ', '(+62) 288 9461 ', 'Ds. Cihampelas No. 232, Administrasi Jakarta Pusat 16374, Kaltara', 'irnanto.uyainah@gmail.com', '$2y$10$0fF4.A6EQQoJ8yPnQ.rCROFxYPDZjDFy.BbfH72nsPcVEXiFNCvyu'),
+('210507777770001', 'Ida  Calista ', '0940 7772 1198', 'Dk. Bambu No. 644, Singkawang 73383, Riau', 'gina.wibisono@sihotang.org', '$2y$10$qCojire/9FnXmieem7dxleoiHkHlhEMWggTCJRwQ46oCAgr4tZcAi'),
+('210508888880001', 'Viman  Kurnia ', '(+62) 840 640 4', 'Gg. Ciwastra No. 626, Sabang 70008, Maluku', 'vera.gunarto@gmail.co.id', '$2y$10$psgQxibzkoML.FE.EQjZLeRLhjilJxitxdmihOP6Kz0Rm/bxW6GJy'),
+('210509999990001', 'Laila  Wijayanti', '0428 5748 312', 'Jln. BKR No. 452, Probolinggo 77888, Lampung', 'irawan.kayun@yuniar.biz.id', '$2y$10$h5YVRROD4Uu8npvX7t7IwOfSOsJv20yc23LwDOXLi.RZpbMmIaF8q'),
 ('8907134586971234', 'ilham Rizqyakbar', '081323465789', 'Sanggrahan, Sleman, Yogyakarta', 'ilham@gmail.com', '$2y$10$a4D5EWpUzq7kyXp8GGvtfO.PjZcFqAzwamZdUOC0P7FAQN0/agwrG'),
 ('8909186957867890', 'dalas nasyar', '081234567890', 'Pogung, Yogyakarta', 'dalas@gmail.com', '$2y$10$jbo2q/pff0qsf7OY2mWLYufUdEx5WxQbxQOGlF7EWLJ.SOr827msi'),
 ('9856784358681234', 'ilham  Rizqyakbar', '081989096789', 'Sanggrahan, Sleman, Kalasan', 'ilham@gmail.com', '$2y$10$662vGTft3cThKvmWsXdowOH3PGAUhRJtkjsqNscZwcyrPpK6drWmC');
@@ -323,7 +483,18 @@ CREATE TABLE `rekening` (
 --
 
 INSERT INTO `rekening` (`NoRekening`, `bank`, `NIK_Pemilik`) VALUES
-('2345095482', 'BCA', '1234890723456789');
+('', 'Pilih Bank', '210509999990001'),
+('231231232312', 'Bank Rakyat Indonesia', '1232132312312'),
+('2345095482', 'BCA', '1234890723456789'),
+('5492104828929651', 'Bank Mandiri', '210501111110001'),
+('5492104828929652', 'Bank Negara Indonesia', '210502222220001'),
+('5492104828929653', 'Bank Mandiri', '210503333330001'),
+('5492104828929654', 'Bank Syariah Indonesia', '210504444440001'),
+('5492104828929655', 'Bank Central Asia', '210505555550001'),
+('5492104828929656', 'Bank Rakyat Indonesia', '210506666660001'),
+('5492104828929657', 'Bank Rakyat Indonesia', '210501212120001'),
+('5492104828929658', 'Bank Negara Indonesia', '210507777770001'),
+('5492104828929659', 'Bank BPD DIY', '210508888880001');
 
 -- --------------------------------------------------------
 
@@ -447,7 +618,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `kost`
 --
 ALTER TABLE `kost`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `penyewaan`
